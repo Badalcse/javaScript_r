c8c8d202f0958c7ab18717e0d8f9cb4e5488d238
Badal kumar <118023518+Badalcse@users.noreply.github.com>
2023-09-28 16:37:54 +0530

    Add files via upload

---
+++ javaScript1.js
@@ -0,0 +1,175 @@
+// const accountId = 14453;
+// let accountEmai = "123@gmail.com";
+
+// var accountPassword = "1234";
+
+// accountCity = "bihar";
+
+// // accountId = 2 not allowed
+
+// console.log(accountId);
+// accountEmai = "badal@123";
+
+// accountPassword = "5432";
+// accountCity = "chennai"
+
+
+
+
+// // console.table([accountEmai, accountPassword, accountCity ])
+
+// name = "kumar"
+
+// console.log(name);
+
+//dataType
+
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let number = Number(score)
+// console.log (typeof number);
+
+let value= 3
+
+let negvalue = -value;
+
+// console.log(negvalue);
+
+let x = 3
+
+let y = ++x
+// console.log(`x: ${x} , y: ${y}`);
+// console.log(x);
+
+//primitive
+// 7 type: String , number, boolean, null, undefined, symbol, BigInt
+
+//Reference(non primitive)
+
+// Array, Object, Function
+
+const Id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(Id);
+// console.log(anotherId);
+
+// console.log(Id === anotherId);
+
+// let userName = {
+//     name: "badal",
+//     email: "bk@123gamil.com"
+// }
+// console.log(userName);
+
+const score = 400;
+// console.log( score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(5));
+
+
+const hundereds = 1000000
+// console.log(hundereds.toExponential());
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.5));
+
+// console.log(Math.min(1,2,3,4,0));
+
+//date and time
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toJSON());
+
+//array
+
+const myArray = [1,2,3,4,5]
+// console.log(typeof myArray);
+// myArray.push(10)
+// console.log(myArray);
+// console.log( myArray.length);
+
+// const myHero = ["badal", "kumar"]
+
+// myArray.unshift(8)
+// console.log(myArray);
+
+// console.log(myArray.indexOf(7));
+
+// const my =myArray.slice(1,3)
+// console.log(my);
+
+// const new1 = myArray.splice(1,3)
+
+// console.log(new1);
+
+const heros = ["thor", "Iroman", "badal"]
+
+const name = ["kumar", "rahul", "vikash"]
+
+heros.push(name)
+// console.log(heros);
+
+// console.log(heros.concat(name));
+
+// console.log([...heros, ...name]);
+// let mysym = Symbol("key")
+
+// let badal = {
+//     name: "kumar",
+//     [mysym]: "rahul",
+//     roll: 123,
+//     depar: "computer"
+// }
+// console.log(typeof badal);
+
+// console.log(badal["roll"]);
+// console.log(badal[mysym]);
+
+
+// const tinterUser = {}
+//     tinterUser.id = "1234abc"
+//     tinterUser.name = "badal"
+//     tinterUser.isLoggedIn = false
+
+//     // console.log(tinterUser);
+
+//     const regularUser = {
+//         email: "123@gmail.com",
+//         username:{
+//             fullName:{
+//                 firstName: "badal",
+//                 lastName: "kumar"
+//             }
+
+//         }
+//     }
+//     console.log(regularUser.username.fullName);
+
+
+// const course = {
+//     coursename: "js in hindi",
+//     price: 999,
+//     courseInstructor: "badal"
+// }
+
+// const{courseInstructor: lm} = course;
+// console.log(lm);
